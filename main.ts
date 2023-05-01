b7659b7c29eb4783fc8a8609107af2c58e57ad3c
Joshua Warren <josh@creatuity.com>
2023-05-01 14:12:33 -0500

    Initial commit.

--- main.ts
+++ main.ts
@@ -1,85 +1,64 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// main.ts
 
-// Remember to rename these classes and interfaces!
+import { Plugin, TFile, TAbstractFile, App, PluginSettingTab, Setting } from 'obsidian';
+import { fetchAndSummarize } from 'podcast-takeaways';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SolomonSettings {
+	openAiApiKey: string;
+	summarizationModel: string;
+	answerModel: string;
+	ffmpegPath: string;
+	folderPath: string;
+	filePathTemplate: string;
+	personalInfo: string;
+	podcastUrls: string[];
+	updateFrequency: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: SolomonSettings = {
+	openAiApiKey: '',
+	summarizationModel: 'gpt-3.5-turbo',
+	answerModel: 'gpt-3.5-turbo',
+	ffmpegPath: '',
+	folderPath: '',
+	filePathTemplate: 'YYYY-MM-DD',
+	personalInfo: '',
+	podcastUrls: [],
+	updateFrequency: 1440, // 1440 minutes = 1 day
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SolomonPlugin extends Plugin {
+	settings: SolomonSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addSettingTab(new SolomonSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => this.fetchAndAppendTakeaways(), this.settings.updateFrequency * 60 * 1000)
+		);
 	}
 
-	onunload() {
+	async fetchAndAppendTakeaways() {
+		for (const url of this.settings.podcastUrls) {
+			const takeaway = await fetchAndSummarize(
+				this.settings.openAiApiKey,
+				this.settings.personalInfo,
+				url,
+				this.settings.ffmpegPath
+			);
 
+			const filePath = this.settings.folderPath + '/' + this.settings.filePathTemplate + '.md';
+			const existingFile = this.app.vault.getAbstractFileByPath(filePath) as TFile;
+
+			if (existingFile) {
+				const existingContent = await this.app.vault.read(existingFile);
+				await this.app.vault.modify(existingFile, existingContent + '\n' + takeaway);
+			} else {
+				await this.app.vault.create(filePath, takeaway);
+			}
+		}
 	}
 
 	async loadSettings() {
@@ -91,46 +70,117 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SolomonSettingTab extends PluginSettingTab {
+	plugin: SolomonPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SolomonPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		let { containerEl } = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Solomon Plugin Settings' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('OpenAI API Key')
+			.setDesc('Enter your OpenAI API key')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your OpenAI API key')
+				.setValue(this.plugin.settings.openAiApiKey)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.openAiApiKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Folder Path')
+			.setDesc('Enter the path where you want the takeaways to be stored')
+			.addText(text => text
+				.setPlaceholder('Enter the path')
+				.setValue(this.plugin.settings.folderPath)
+				.onChange(async (value) => {
+					this.plugin.settings.folderPath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('File Path Template')
+			.setDesc('Enter the file path template with placeholders for date information (e.g., YYYY-MM-DD)')
+			.addText(text => text
+				.setPlaceholder('Enter the file path template')
+				.setValue(this.plugin.settings.filePathTemplate)
+				.onChange(async (value) => {
+					this.plugin.settings.filePathTemplate = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Personal Information')
+			.setDesc('Enter the personal information you would like GPT to personalize your takeaways for')
+			.addTextArea(text => text
+				.setPlaceholder('Enter your personal information')
+				.setValue(this.plugin.settings.personalInfo)
+				.onChange(async (value) => {
+					this.plugin.settings.personalInfo = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Podcast URLs')
+			.setDesc('Enter one or more podcast URLs to check for new episodes')
+			.addTextArea(text => text
+				.setPlaceholder('Enter podcast URLs, separated by commas')
+				.setValue(this.plugin.settings.podcastUrls.join(', '))
+				.onChange(async (value) => {
+					this.plugin.settings.podcastUrls = value.split(',').map(url => url.trim());
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Update Frequency')
+			.setDesc('Enter the frequency to check for updates (in minutes)')
+			.addSlider(slider => slider
+				.setLimits(1, 1440, 1) // From 1 minute to 1440 minutes (1 day)
+				.setValue(this.plugin.settings.updateFrequency)
+				.setDynamicTooltip()
+				.onChange(async (value) => {
+					this.plugin.settings.updateFrequency = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('FFMpeg Path')
+			.setDesc('Optional: Enter the path to FFMpeg on your system')
+			.addText(text => text
+				.setPlaceholder('Enter the path to FFMpeg')
+				.setValue(this.plugin.settings.ffmpegPath)
+				.onChange(async (value) => {
+					this.plugin.settings.ffmpegPath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Summarization Model')
+			.setDesc('Enter the OpenAI model for summarization (default: gpt-3.5-turbo)')
+			.addText(text => text
+				.setPlaceholder('Enter the summarization model')
+				.setValue(this.plugin.settings.summarizationModel)
+				.onChange(async (value) => {
+					this.plugin.settings.summarizationModel = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Takeaways Model')
+			.setDesc('Enter the OpenAI model for takeaways (default: gpt-3.5-turbo)')
+			.addText(text => text
+				.setPlaceholder('Enter the takeaways model')
+				.setValue(this.plugin.settings.answerModel)
+				.onChange(async (value) => {
+					this.plugin.settings.answerModel = value;
 					await this.plugin.saveSettings();
 				}));
 	}
